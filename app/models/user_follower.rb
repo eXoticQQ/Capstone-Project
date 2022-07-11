7d1644cc34d6ad84aef830f857882da1e985dd42
Vitalii <vetal.shtager@gmail.com>
2022-07-11 12:30:18 -0400

    custom model methods to see user followers

--- app/models/user_follower.rb
+++ app/models/user_follower.rb
@@ -1,3 +1,12 @@
 class UserFollower < ApplicationRecord
     belongs_to :user
+
+    def self.user_and_followers
+        self.all.map do |user|
+            {
+                user: user.id,
+                follower_id: user.follower_id
+            }
+        end
+    end
 end
